fedfe278fefa227bba07ef48da028d9760318536
André Adriano <andre@dev-next.com>
2020-01-13 13:05:46 -0300

    ✨ Created a slot on EventCard Cmponent

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -3,8 +3,7 @@
     <section class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <BaseIcon name="users" />
-      <span>{{ event.attendees.length }} attending</span>
+      <base-icon name="users">{{ event.attendees.length }} attending</base-icon>
     </section>
   </router-link>
 </template>
