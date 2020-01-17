fe5aa189cb14a3ec6ce4c661c2aa9fa9957858d2
André Adriano <andre@dev-next.com>
2020-01-17 09:33:39 -0300

    ✨ Created BaseSelect Component

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="field">
+    <label v-if="label">{{ label }}</label>
+    <select :value="value" @input="updateValue" v-bind="$attrs">
+      <option
+        v-for="option in options"
+        :selected="option === value"
+        :key="option">
+          {{ option }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+
+  props: {
+    label: {
+      type: String,
+    },
+
+    options: {
+      type: Array,
+      required: true,
+    },
+
+    value: {
+      type: [String, Number],
+    },
+  },
+
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value);
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -3,10 +3,11 @@
     <h1>Create Event</h1>
 
     <form @submit.prevent="createEvent">
-      <label>Select a category</label>
-      <select v-model="event.category">
-        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
-      </select>
+      <base-select
+        v-model="event.category"
+        label="Select a category"
+        :options="categories"
+      />
 
       <h3>Name & describe your event</h3>
       <base-input
@@ -32,12 +33,13 @@
         <label>Date</label>
         <datepicker v-model="event.date" placeholder="Select a date"/>
       </div>
-      <div class="field">
-        <label>Select a time</label>
-        <select v-model="event.time">
-          <option v-for="time in times" :key="time">{{ time }}</option>
-        </select>
-      </div>
+
+      <base-select
+        v-model="event.time"
+        label="Select a time"
+        :options="times"
+      />
+
       <input type="submit" class="button -fill-gradient" value="Submit"/>
     </form>
   </article>
